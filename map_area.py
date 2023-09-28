efd4ba76bb75943470e7a6003aaaa2c672c87d94
zizo1964 <93069541+zizo1964@users.noreply.github.com>
2023-09-29 02:46:12 +0800

    Update map_area.py

--- map_area.py
+++ map_area.py
@@ -120,6 +120,11 @@ if __name__ == '__main__':
 
     # Save the map with markers and basic popups to an HTML file
     map_my.save('itp_area_map.html')
+
+    # Display the HTML file in Streamlit
+    with open('itp_area_map.html', 'r', encoding='utf-8') as map_file:
+        map_html = map_file.read()
+        components.html(map_html, 1000, 600)
    
 
     # for itp_data in itp_list_state.to_dict(orient='records'):
